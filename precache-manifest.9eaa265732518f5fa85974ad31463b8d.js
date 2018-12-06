b522a9acd3407ce8820174fb424dc5690f16cc46
David <davidcondori99@gmail.com>
2018-12-06 13:28:44 -0500

    Updates

---
+++ precache-manifest.9eaa265732518f5fa85974ad31463b8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d90a2d9c680b7843b8eb20ae8add0917",
+    "url": "/React-Router/static/media/image1.d90a2d9c.jpg"
+  },
+  {
+    "revision": "061c2457bcdd59d9e0cb",
+    "url": "/React-Router/static/js/runtime~main.061c2457.js"
+  },
+  {
+    "revision": "1b68370629b4339b347e",
+    "url": "/React-Router/static/js/main.1b683706.chunk.js"
+  },
+  {
+    "revision": "0332bc5021bd971eff37",
+    "url": "/React-Router/static/js/1.0332bc50.chunk.js"
+  },
+  {
+    "revision": "1b68370629b4339b347e",
+    "url": "/React-Router/static/css/main.63f59e30.chunk.css"
+  },
+  {
+    "revision": "972d54518a31e86ced05e7280fcc0b5f",
+    "url": "/React-Router/index.html"
+  }
+];

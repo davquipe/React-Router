ad702b5182a12ca3cc53fa97a3d69b92c435d180
David <davidcondori99@gmail.com>
2018-12-06 13:08:53 -0500

    ejemplo de un sitio web con React, Route

---
+++ docs/precache-manifest.c635a7fd93a49b9ac2578d3c645d994e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d90a2d9c680b7843b8eb20ae8add0917",
+    "url": "/static/media/image1.d90a2d9c.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fad8bfaf6d92531d037c",
+    "url": "/static/js/main.fad8bfaf.chunk.js"
+  },
+  {
+    "revision": "0332bc5021bd971eff37",
+    "url": "/static/js/1.0332bc50.chunk.js"
+  },
+  {
+    "revision": "fad8bfaf6d92531d037c",
+    "url": "/static/css/main.c39ce521.chunk.css"
+  },
+  {
+    "revision": "f2eec5f029c77a5f6f95f1269b5e13ab",
+    "url": "/index.html"
+  }
+];
